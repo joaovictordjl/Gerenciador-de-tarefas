771e608b01285ff85016ef5b5ce11a4b34c1a153
João <victorjhon779@gmail.com>
2024-01-16 20:41:30 -0300

    criado controlador de testes

---
+++ src/main/java/com/Gerenciador/de/tarefas/controller/TesteApiController.java
@@ -0,0 +1,14 @@
+package com.Gerenciador.de.tarefas.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TesteApiController {
+
+    @GetMapping("/teste-api")
+    private String teste(){
+        return "Sucesso";
+    }
+}
